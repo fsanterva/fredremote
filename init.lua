55a9669eac7a4cdb6e1265c296b3107fa6105515
codepen24 <codepen24@protonmail.com>
2024-01-14 17:13:28 +0800

    updated

--- init.lua
+++ init.lua
@@ -353,13 +353,15 @@ vim.api.nvim_set_keymap("v", "<leader>zz", "zd", {noremap = true})
 vim.api.nvim_set_keymap("n", "<Space>", "za", {noremap = true})
 vim.api.nvim_set_keymap("v", "<2-RightMouse>", "zf", {noremap = true})
 vim.api.nvim_set_keymap("n", "<2-LeftMouse>", "vit", {noremap = true})
-vim.api.nvim_set_keymap("n", "<leader>ff", ":vimgrep /xxxxxxxx/gj **/*", {noremap=true})
 vim.api.nvim_set_keymap("n", "<leader>cp", ":copen<CR>", {noremap = true})
 vim.api.nvim_set_keymap("n", "<Backspace>", [[:e#<CR>]] ,{})
 vim.api.nvim_set_keymap("n", "<leader>o", "<S-$>", {noremap = true})
 vim.api.nvim_set_keymap("v", "<leader>o", "<S-$>", {noremap = true})
 vim.api.nvim_set_keymap("n", "<leader>i", "0", {noremap = true})
+
 vim.api.nvim_set_keymap("i", "jj", "<Esc><Esc>", {noremap=true})
+vim.api.nvim_set_keymap('t', 'jj', [[<C-\><C-n>]], { noremap = true })
+
 vim.api.nvim_set_keymap("n", "<leader>ss", ":w<CR>", {noremap = true})
 vim.api.nvim_set_keymap("i", "<leader>ss", ":w<CR>", {noremap = true})
 vim.api.nvim_set_keymap("n", "<leader><Space>", ":bd<CR>", {noremap = true})
@@ -369,22 +371,24 @@ vim.api.nvim_set_keymap("n", "<leader>vv", "vit", {noremap = true})
 vim.api.nvim_set_keymap("n", "<leader>llc", ":%bd | c#<CR>", {noremap = true})
 vim.api.nvim_set_keymap("n", "<leader>end", ":qall!<CR>", {noremap = true})
 vim.api.nvim_set_keymap("n", "<leader>vim", ":e ~/.config/nvim/init.lua<CR>", {noremap=true})
-vim.api.nvim_set_keymap("n", "<leader>w", ":lua Ww()<CR>", {noremap = true})
-vim.api.nvim_set_keymap("n", "<leader>h", ":lua Wwh()<CR>", {noremap = true})
-vim.api.nvim_set_keymap("n", "<leader>vh", ":lua Wwvh()<CR>", {noremap = true})
 vim.api.nvim_set_keymap("n", "<leader>vit", "vit", {noremap = true})
 
 vim.api.nvim_set_keymap("n", "<leader>pth", ":echo expand('%')<CR>", {noremap = true})
 vim.api.nvim_set_keymap('n', '<leader>yth', [[:let @+ = expand('%') | call system('echo "' . @+ . '" | xclip -i -selection clipboard')<CR>]], { noremap = true, silent = true })
 
-vim.api.nvim_set_keymap("n", "<leader>rep", ":%s /xxxxxx/xxxxxx/g", {noremap=true})
 vim.api.nvim_set_keymap("n", "<leader>cvv", "<C-v>", {noremap=true})
+
 vim.api.nvim_set_keymap("n", "<leader>tn", ":tabnew<CR>", {noremap=true})
 vim.api.nvim_set_keymap("n", "<Tab>", ":tabn<CR>", {noremap=true})
 vim.api.nvim_set_keymap("n", "<S-Tab>", ":tabp<CR>", {noremap=true})
+
 vim.api.nvim_set_keymap("n", "<C-y>", ":vertical resize +20<CR>", {noremap=true})
 vim.api.nvim_set_keymap("n", "<C-t>", ":vertical resize -20<CR>", {noremap=true})
-vim.api.nvim_set_keymap("n", "<leader>nf", ":cfdo %s/xxxxx/xxxxx/g | :cfdo update", {noremap=true})
+
+vim.api.nvim_set_keymap("n", "<leader>rep", ":%s /xxxxxx/xxxxxx/g", {noremap=true})
+
+vim.api.nvim_set_keymap("n", "<leader>ff", ":vimgrep /xxxxxxxx/gj **/*", {noremap=true})
+vim.api.nvim_set_keymap("n", "<leader>nf", ":cfdo %s/xxxxx/xxxxx/g", {noremap=true})
 vim.api.nvim_set_keymap("n", "<leader>nfu", ":cfdo update", {noremap=true})
 
 vim.cmd("augroup ProjectDrawer")
@@ -393,6 +397,7 @@ vim.cmd("augroup ProjectDrawer")
 -- vim.cmd("autocmd VimEnter * :Lex")
 vim.cmd("augroup END")
 
+vim.api.nvim_set_keymap("n", "<leader>w", ":lua Ww()<CR>", {noremap = true})
 function Ww()
   if vim.fn.has('nvim') then
     vim.cmd("cd /mnt/c/wamp64/www/devs/dilate/wp")
@@ -401,6 +406,7 @@ function Ww()
   end
 end
 
+vim.api.nvim_set_keymap("n", "<leader>h", ":lua Wwh()<CR>", {noremap = true})
 function Wwh()
   if vim.fn.has('nvim') then
     vim.cmd("cd /mnt/c/Windows/system32/drivers/etc/")
@@ -408,6 +414,7 @@ function Wwh()
   end
 end
 
+vim.api.nvim_set_keymap("n", "<leader>vh", ":lua Wwvh()<CR>", {noremap = true})
 function Wwvh()
   if vim.fn.has('nvim') then
     vim.cmd("cd /mnt/c/wamp64/bin/apache/apache2.4.58/conf/extra/")
@@ -552,6 +559,5 @@ vim.opt.autochdir = true -- Set autochdir
 vim.cmd('autocmd BufEnter * silent! lcd %:p:h') -- Define autocmd for changing directory to the file's directory on BufEnter
 
 -- Set 'jjj' to enter terminal mode
-vim.api.nvim_set_keymap('t', 'jj', [[<C-\><C-n>]], { noremap = true })
 
 -- See `:help vim.keymap.set()`
